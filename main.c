aa6ab2892b782bd80a0ea98c69c94adb46ffee18
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-10-08 16:42:10 -0300

    called the function to store the arg vars and changed main loop for readline

--- main.c
+++ main.c
@@ -12,25 +12,50 @@
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int ac, char **av, char **envp)
 {
+	t_envp_lst	*env_vars; // variable declared to store environment variables
 	char	*line;
 	char	*prompt;
 
-	line = malloc(sizeof(short));
-	if (line == NULL)
-		ft_error();
-	while (line != NULL)
+	(void)ac;
+	(void)av;
+	env_vars = store_envp(envp); // get original envp and store in our own ->
+								 // linked list to be able to delete and ->
+								 // add new variables
+	/* this while is here to be able to visualize our own envp:
+	 * int i = 0;
+	while (lst_env_args)
+	{
+		//printf("[node %d]\t[var = %s]\t[value = %s]\n", i, lst_env_args->var, lst_env_args->value);
+		lst_env_args = lst_env_args->next;
+		i++;
+	}*/
+//	line = malloc(sizeof(short));
+//	if (line == NULL)
+//		ft_error();
+	while (1)
 	{
 		prompt = get_prompt();
-		add_to_mem_list("prompt\n", prompt);
-		free(line);
+		add_to_mem_list("prompt\n", prompt); // ??
 		line = readline(prompt);
 		add_history(line);
 		check_single_quote(&line);
 		check_double_quote(&line);
-		execute_line(line);
+		execute_line(line, *env_vars);
 		free(prompt);
 	}
-	free(prompt);
+	//while (line != NULL)
+	//{
+	//	prompt = get_prompt();
+	//	add_to_mem_list("prompt\n", prompt); // ??
+	//	free(line);
+	//	line = readline(prompt);
+	//	add_history(line);
+	//	check_single_quote(&line);
+	//	check_double_quote(&line);
+	//	execute_line(line);
+	//	free(prompt);
+	//}
+	//free(prompt);
 }
