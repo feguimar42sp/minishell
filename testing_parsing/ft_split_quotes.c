324a0efb7a9dceb72b86e12b23c1a6bccb3ece1f
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-10-17 15:24:02 -0300

    modified name of variable libft.h minishell.h and added new file

--- testing_parsing/ft_split_quotes.c
+++ testing_parsing/ft_split_quotes.c
@@ -10,25 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct	s_args_lst
+typedef struct s_args_lst
 {
-	char				*arg; // single argument
-	int					type; // type of argument - need to be implemented
-	struct s_args_lst	*next;// points to the next argument
-}						t_args_lst;
+	char *arg;               // single argument
+	int type;                // type of argument - need to be implemented
+	struct s_args_lst *next; // points to the next argument
+}			t_args_lst;
 
-typedef struct	s_quotes
+typedef struct s_quotes
 {
-	int	single_count;
-	int	double_count;
-}		t_quotes;
+	int		single_count;
+	int		double_count;
+}			t_quotes;
 
-enum e_args
+enum		e_args
 {
 	STRING,
 	OPERATORS,
@@ -109,12 +109,12 @@ void	create_node(t_args_lst **args_lst, const char *str, int start, int len)
 	temp = *args_lst;
 	new->arg = ft_substr(str, start, len);
 	new->type = STRING;
-	//printf("string = [%s]\n", ft_substr(str, start, len));
+	// printf("string = [%s]\n", ft_substr(str, start, len));
 	if (temp == NULL)
 		*args_lst = new;
 	else
 	{
-	 	while (temp->next != NULL)
+		while (temp->next != NULL)
 		{
 			temp = temp->next;
 		}
@@ -126,16 +126,21 @@ int	separate_dquotes(char *s, t_args_lst **split, bool *d_quotes)
 {
 	int	i;
 	int	len;
+	int	size;
+
+	size = ft_strlen(s);
 	// bool d_quotes is received as true, because it has found one quote
 	// however it's missing its other half
 	len = find_pos_strchr(s + 1, '\"');
 	if (len != -1)
 		*d_quotes = !(*d_quotes);
 	i = 0;
-	len += 2;
+	len += 1;
+	if (len > size)
+		len = size;
 	if (s[i] != '\0')
 	{
-		create_node(split, s, i, len);
+		create_node(split, s, i, len + 1);
 		i = i + len;
 	}
 	return (i);
@@ -145,57 +150,72 @@ int	separate_squotes(char *s, t_args_lst **split, bool *s_quotes)
 {
 	int	i;
 	int	len;
+	int	size;
 
+	size = ft_strlen(s);
 	len = find_pos_strchr(s + 1, '\'');
 	if (len != -1)
 		*s_quotes = !(*s_quotes);
 	i = 0;
-	len += 2;
+	len += 1;
+	if (len > size)
+		len = size;
 	if (s[i] != '\0')
 	{
-		create_node(split, s, i, len);
+		create_node(split, s, i, len + 1);
 		i = i + len;
 	}
+	// printf("s[i] = [%c]\n", s[i]);
 	return (i);
+}
+
+int	separator_by_length(char *s)
+{
+	int	closest;
+	int	size;
+
+	size = ft_strlen(s);
+	closest = find_pos_strchr(s + 1, ' '); // -1
+	if (find_pos_strchr(s + 1, '\'') < closest && find_pos_strchr(s + 1, '\'') != -1)
+		closest = find_pos_strchr(s + 1, '\'');
+	if (find_pos_strchr(s + 1, '\"') < closest)
+		closest = find_pos_strchr(s + 1, '\"');
+	if (closest > size)
+		closest = size;
+	return (closest);
 }
 
 int	separate_spaces(char *s, t_args_lst **split)
 {
 	int	i;
 	int	len;
+	int	size;
 
-	// len = find_pos_strchr(s + 1, '\''); find a single quote inside the str
-	// len = find_pos_strchr(s + 1, '\''); find a double quote inside the str
-
-	// ERROR need a string that would theorically end with a space to be able to do this:
-	if (find_pos_strchr(s + 1, '\'') > 0)
-		len = find_pos_strchr(s + 1, '\''); 
-	else if (find_pos_strchr(s + 1, '\"') > 0)
-		len = find_pos_strchr(s + 1, '\"');
-	else
-	{
-		len = find_pos_strchr(s + 1, ' ');
-		len += 1;
-	}
+	size = ft_strlen(s);
+	len = separator_by_length(s);
+	printf("char found at pos %d = [%c]\n", len, s[len]);
+	/*len = find_pos_strchr(s + 1, ' ');
+	if (find_pos_strchr(s + 1, '\'') < len)
+		len = find_pos_strchr(s + 1, '\'');
+	else if (find_pos_strchr(s + 1, '\"') < len)
+		len = find_pos_strchr(s + 1, '\"');*/
+	len += 1;
 	// if there's a quote after one or a few non-space was found,->
 	// then check if there's another quote that matches it in the rest of the str
-	// if there's a quote, the goes back to the main function with the char 'quote'
-	// as position, so it can go back and enter another if that it meets the requirement to
+	// if there's a quote,
+	// the goes back to the main function with the char 'quote'
+	// as position,
+	// so it can go back and enter another if that it meets the requirement to
 	// separate it as a single string
 	i = 0;
-	//len += 1;
-	//printf("len spaces = [%d]\n", len);
-	if (s[i] != '\0')
-	{
-		create_node(split, s, i, len);
-		i = i + len;
-	}
+	create_node(split, s, i, len);
+	i = i + len;
 	return (i);
 }
 
 int	quotes_count(char *s)
 {
-	int			i;
+	int		i;
 	bool	s_quotes;
 	bool	d_quotes;
 
@@ -223,10 +243,10 @@ int	quotes_count(char *s)
 t_args_lst	*ft_lst_split(char *s)
 {
 	t_args_lst	*split;
-	int		i;
-	int		value;
-	bool	s_quotes;
-	bool	d_quotes;
+	int			i;
+	int			value;
+	bool		s_quotes;
+	bool		d_quotes;
 
 	split = NULL;
 	i = 0;
@@ -240,28 +260,25 @@ t_args_lst	*ft_lst_split(char *s)
 	}
 	while (s[i] != '\0')
 	{
-		// ERROR ip spaces only until a single or double quote or a space is found
+		// ERROR if spaces only until a single or double quote or a space is found
 		while (s[i] != '\'' && s[i] != '\"' && s[i] == ' ' && s[i] != '\0')
 			i++;
+		//printf("char at pos %d(i) in str = [%c]\n", i, s[i]);
 		if (s[i] == '\'')
 		{
 			s_quotes = !s_quotes;
-			value = separate_squotes(s + i, &split, &s_quotes);	
-			i += value + 1;	
+			value = separate_squotes(s + i, &split, &s_quotes);
 		}
 		else if (s[i] == '\"')
 		{
 			d_quotes = !d_quotes;
 			value = separate_dquotes(s + i, &split, &d_quotes);
-			i += value + 1;
 		}
 		else if (s[i] != ' ' && s[i] != '\0')
-		{
-			value = separate_spaces(s + i, &split);	
-			i += value + 1;
-		}
-	/*	else if (s[i] != '\0')
-			i++;*/
+			value = separate_spaces(s + i, &split);
+		if (value >= ft_strlen(s))
+			break ;
+		i += value + 1;
 	}
 	printf("end of function ft_lst_split\n");
 	return (split);
@@ -269,18 +286,20 @@ t_args_lst	*ft_lst_split(char *s)
 
 int	main(void)
 {
-  t_args_lst	*split;
-  char			*str;
+	t_args_lst	*split;
+	char		*str;
+	int			i;
 
-  str = "echo        i \"sa'brina\"         'i 'ola'          FF'G\"G '  ola\"";
-  printf("[%s]\n", str);
-  split = ft_lst_split(str);
-
-  int i = 0;
-  while (split)
-  {
-	printf("node[%d] - split->arg:[%s]\n", i, split->arg);
-	split = split->next;
-	i++;
-  }
+	// str = "echo        i \"sa'brina\"         'i 'ola'          FF'G\"G '  ola\"";
+	// str = "echo i sa brina i ola FF G G ola";
+	str = "'i'ola'  ff'gg\'\"\'";
+	printf("[%s]\n", str);
+	split = ft_lst_split(str);
+	i = 0;
+	while (split->next)
+	{
+		printf("node[%d] - split->arg:[%s]\n", i, split->arg);
+		split = split->next;
+		i++;
+	}
 }
