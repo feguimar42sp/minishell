e9fb6942e8f940b98aa3872e1b7f7799632a8b70
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-10-21 14:01:23 -0300

    parsing working with redirects inbetween spaces, but not within strings

--- testing_parsing/split_versionfinal.c
+++ testing_parsing/split_versionfinal.c
@@ -10,10 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 typedef struct s_args_lst
 {
@@ -76,124 +76,101 @@ void	create_node(t_args_lst **args_lst, const char *str, int start, int len)
 	temp = *args_lst;
 	new->arg = ft_substr(str, start, len);
 	new->type = STRING;
-	// printf("string = [%s]\n", ft_substr(str, start, len));
 	if (temp == NULL)
 		*args_lst = new;
 	else
 	{
 		while (temp->next != NULL)
-		{
 			temp = temp->next;
-		}
 		temp->next = new;
 	}
 }
 
-char	*ft_strchr(const char *s, int c)
-{
-	char	*ret;
-
-	ret = (char *)s;
-	while (1)
-	{
-		if (*ret == (char)c)
-			return (ret);
-		if (*ret == '\0')
-			break ;
-		ret++;
-	}
-	return (NULL);
-}
-
-size_t	ft_strcspn(const char *s, const char *reject)
-{
-	size_t	len;
-	size_t	j;
-
-	if (!s || !reject)
-		return (0);
-	len = 0;
-	while (s[len])
-	{
-		j = 0;
-		while (reject[j])
-		{
-			if (s[len] == reject[j])
-				return (len);
-			j++;
-		}
-		len++;
-	}
-	return (len);
-}
-
-int	split_by_quotes(t_args_lst **split, char *str, int i)
+void	split_by_quotes(t_args_lst **split, char *str, int *i)
 {
 	char	quote;
 	int		len;
 	int		start;
 
 	len = 0;
-	start = i;
-	if (str[start] == '\'')
-		quote = '\'';
-	else if (str[start] == '\"')
-		quote = '\"';
+	start = *i;
+	if (str[start] == '\'' || str[start] == '\"')
+		quote = str[start];
 	start++;
+	len++;
 	while (str[start] != quote && str[start] != '\0')
 	{
 		start++;
 		len++;
 	}
-	len += 2;
-	create_node(split, str, i, len);
-	return(i + len);
+	len += 1;
+	create_node(split, str, *i, len);
+	*i = *i + len;
+}
+
+void	split_by_redirects(t_args_lst **split, char *str, int *i)
+{
+	char	redirect;
+	int		len;
+	int		start;
+
+	len = 0;
+	start = *i;
+	if (str[start] == '<')
+	{
+		while (str[start] == '<' && str[start] != '\0')
+		{
+			start++;
+			len++;
+		}
+	}
+	else if (str[start] == '>')
+	{
+		while (str[start] == '>' && str[start] != '\0')
+		{
+			start++;
+			len++;
+		}
+	}
+	//len += 1;
+	create_node(split, str, *i, len);
+	*i = *i + len;
 }
 
 t_args_lst	*ft_lst_split(char *str)
 {
 	int			i;
 	int			start;
-	int			end;
+	int			len;
 	t_args_lst	*split;
 
 	split = NULL;
 	i = 0;
 	start = 0;
-	end = 0;
+	len = 0;
 	while (str[i])
 	{
-		while ((str[i] == ' ' || str[i] == '\t') && str[i] != '\0') // skip space and tab
-		{
-			printf("spaces skipped\n");
+		while ((str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
 			i++;
-		}
-		if (str[i] == '\'')
+		if (str[i] == '\'' || str[i] == '\"')
+			split_by_quotes(&split, str, &i);
+		else if (str[i] == '<' || str[i] == '>')
 		{
-			printf("single quote found\n");
-			i = split_by_quotes(&split, str, i);
-			printf("value of i 0[%d]\n", i);
-		}
-		else if (str[i] == '\"')
-		{
-			printf("double quote found\n");
-			i = split_by_quotes(&split, str, i);
-			printf("value of i 1[%d]\n", i);
+			printf("str[i] == [%c]\n", str[i]);
+			split_by_redirects(&split, str, &i);
 		}
 		else
 		{
-			end = 0;
+			len = 0;
 			start = i;
-			while (str[i] != ' ' && str[i] != '\t' && str[i] != '\'' && str[i] != '\"' && str[i] != '\0')
+			while (str[i] != ' ' && str[i] != '\t' && str[i] != '\''
+				&& str[i] != '\"' && str[i] != '\0')
 			{
 				i++;
-				end++;
+				len++;
 			}
-			create_node(&split, str, start, end);
+			create_node(&split, str, start, len);
 		}
-		if (str[i] == '\0')
-			break ;
-		printf("here\n");
 	}
 	return (split);
 }
@@ -206,7 +183,9 @@ int	main(int ac, char **av)
 	int			i;
 
 	//str = "echo \"p\"a\'\'\'\'\'ABCDE\"F\"G\'\"ra\"\'b\'\"\"\'\'\"\"\'\'\'ens\'";
-	str = av[1];
+	//str = "echo \"Hello, World!\" < greetings.txt";
+	str = "command > all_output.txt 2>&1";
+	// str = av[1];
 	printf("[%s]\n", str);
 	split = ft_lst_split(str);
 	i = 0;
