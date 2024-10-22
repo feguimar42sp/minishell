8f134eec58440a04b3b25f55a6d38e5ea49d961c
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-10-22 16:05:09 -0300

    code updated to work with a mix of quotes and how bash treats thses command with this mix of arguments

--- testing_parsing/split_versionfinal.c
+++ testing_parsing/split_versionfinal.c
@@ -70,7 +70,6 @@ void	create_node(t_args_lst **args_lst, const char *str, int start, int len)
 {
 	t_args_lst	*new;
 	t_args_lst	*temp;
-	enum e_args	type;
 
 	new = (t_args_lst *)malloc(sizeof(t_args_lst));
 	temp = *args_lst;
@@ -86,7 +85,7 @@ void	create_node(t_args_lst **args_lst, const char *str, int start, int len)
 	}
 }
 
-void	split_by_quotes(t_args_lst **split, char *str, int *i)
+/*void	split_by_quotes(t_args_lst **split, char *str, int *i)
 {
 	char	quote;
 	int		len;
@@ -104,6 +103,46 @@ void	split_by_quotes(t_args_lst **split, char *str, int *i)
 	{
 		start++;
 		len++;
+	}
+	len += 1;
+	printf("str[start] = %c and str[start + 1] = %c\n", str[start], str[start + 1]);
+	create_node(split, str, *i, len);
+	*i = *i + len;
+}
+*/
+
+void	split_by_quotes(t_args_lst **split, char *str, int *i)
+{
+	char	quote;
+	int		len;
+	int		start;
+
+	len = 0;
+	start = *i;
+	if (str[start] == '\'' || str[start] == '\"')
+	{
+		quote = str[start];
+		start++;
+		len++;
+	}
+	while (str[start] != '\0')
+	{
+		while (str[start] != quote && str[start] != '\0')
+		{
+			start++;
+			len++;
+		}
+		if (str[start + 1] == ' ' || str[start + 1] == '<'
+			|| str[start + 1] == '>' || str[start + 1] == '|'
+			|| str[start + 1] == '\0')
+		{
+			break ;
+		}
+		else
+		{
+			start++;
+			len++;
+		}
 	}
 	len += 1;
 	create_node(split, str, *i, len);
@@ -129,7 +168,12 @@ void	split_by_redirects(t_args_lst **split, char *str, int *i)
 	create_node(split, str, *i, len);
 	*i = *i + len;
 }
-
+/*
+void	split_by_dollarsign(t_args_lst **split, char *str, int *i)
+{
+	
+}*/
+/*
 void	split_by_spaces(t_args_lst **split, char *str, int *i)
 {
 	int	len;
@@ -149,30 +193,72 @@ void	split_by_spaces(t_args_lst **split, char *str, int *i)
 	*i = *i + len;
 	if ((str[start] == '>' || str[start] == '<' || str[start] == '|')
 		&& str[start] != '\0')
-	{
 		split_by_redirects(split, str, i);
+}*/
+
+void	split_by_spaces(t_args_lst **split, char *str, int *i)
+{
+	int	len;
+	int	start;
+
+	len = 0;
+	start = *i;
+		//&& str[start] != '\'' && str[start] != '\"'
+	while (str[start] != ' ' && str[start] != '\t'
+		&& str[start] != '\0' && str[start] != '<'
+		&& str[start] != '>' && str[start] != '|')
+	{
+		start++;
+		len++;
 	}
+	create_node(split, str, *i, len);
+	*i = *i + len;
+	if ((str[start] == '>' || str[start] == '<' || str[start] == '|')
+		&& str[start] != '\0')
+		split_by_redirects(split, str, i);
+}
+
+bool	unclosed_quotes(char *str)
+{
+	int		i;
+	bool	s_quotes;
+	bool	d_quotes;
+
+	i = 0;
+	s_quotes = false;
+	d_quotes = false;
+	while (str[i])
+	{
+		if (str[i] == '\'' && d_quotes == false)
+			s_quotes = !s_quotes;
+		if (str[i] == '\"' && s_quotes == false)
+			d_quotes = !d_quotes;
+		i++;
+	}
+	if (s_quotes || d_quotes)
+		return (false);
+	return (true);
 }
 
 t_args_lst	*ft_lst_split(char *str)
 {
 	int			i;
-	int			start;
-	int			len;
 	t_args_lst	*split;
 
 	split = NULL;
 	i = 0;
-	start = 0;
-	len = 0;
+	if (!unclosed_quotes(str))
+		return (NULL);
 	while (str[i])
 	{
 		while ((str[i] == ' ' || str[i] == '\t') && str[i] != '\0')
 			i++;
 		if (str[i] == '\'' || str[i] == '\"')
 			split_by_quotes(&split, str, &i);
-		else if (str[i] == '<' || str[i] == '>' || str[start] == '|')
+		else if (str[i] == '<' || str[i] == '>' || str[i] == '|')
 			split_by_redirects(&split, str, &i);
+//		else if (str[i] == '$')
+//			split_by_dollarsign(&split, str, &i);
 		else
 			split_by_spaces(&split, str, &i);
 	}
@@ -182,17 +268,23 @@ t_args_lst	*ft_lst_split(char *str)
 int	main(int ac, char **av)
 {
 	t_args_lst	*split;
-	t_args_lst	*new_split;
 	char		*str;
 	int			i;
 
-	str = "echo \"p\"a\'\'\'\'\'ABCDE\"F\"G\'\"ra\"\'b\'\"\"\'\'\"\"\'\'\'ens\'";
-	// str = "echo \"Hello, World!\" < greetings.txt";
+	//str = "echo \"p\"a\'\'\'\'\'ABCDE\"F\"G\'\"ra\"\'b\'\"\"\'\'\"\"\'\'\'ens\'";
+	//str = "echo \"Hello, World!\" < greetings.txt";
 	// str = "command >> all_  \"olaola\" | output.txt 2<<&1>";
-	// str = "ifconfig|grep \'inet \'|awk \'{print $2}\'";
+	str = "ifconfig|grep \'inet \'\"dfsddsfsd\"|awk \'{print $2}\'";
 	// str = "\'inet \'|awk";
-	// str = "\'inet \'>>awk";
-	// str = av[1];
+	//str = "\'inet \'>>awk";
+	//str = "\'inet >>awk";
+	// str = "echo "This will" 'be' wr'i't't'en" to file">"myfile"";
+	//str = av[1];
+	//str = "echo \"p\"a\'\'\'\'\'\'\"ra\"\'b\'\"\"\'\'\"\"\'\'\'éns\'";
+	//str = "echo \"\"\"\"\"\"T\'u\'d\"o\"                      b\'\'\'\'\"e\"\'\'\"\"\"\"\"\"\'m\'\"\"\"\"\"\"\'?\'";
+	//str = "echo \"List of files:\">files.txt | cat files.txt";
+	//str = "echo \"\"\"\"\"\"T\'u\'d\"o b\"\'\'\'\'\"e\"\'\'\"\"\"\"\"\"\'m\'\"\"\"\"\"\"\'?'";
+
 	printf("[%s]\n", str);
 	split = ft_lst_split(str);
 	i = 0;
