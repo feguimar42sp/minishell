98ca007df2285e9291cc71d387ecc345c61341cb
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-11-02 23:17:44 -0300

    trying to put it according to the norm

--- env_vars/expand_environment_vars.c
+++ env_vars/expand_environment_vars.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   var_expansion.c                                    :+:      :+:    :+:   */
+/*   expand_environment_vars.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sabrifer <sabrifer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:00:57 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/11/02 20:01:02 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/11/02 22:37:04 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,36 +50,86 @@ char	*parse_var_found(char *str)
 	return (var_found);
 }
 
-void	handle_environment_vars_expansion(t_args_lst **arg_lst)
+char	*get_var_value(char *var)
 {
-	char	*expanded_var;
-	char	*var;
 	char	*value;
+
+	value = ft_getenv(var);
+	if (value == NULL)
+		value = ft_strdup("");
+	return (value);
+}
+	
+char	*join_prefix_and_value(char *s1, char *s2)
+{
+	char	*ret;
+
+	ret = ft_strjoin(s1, s2);
+	if (!ret)
+	{
+		free (s1);
+		free (s2);
+		return (NULL);
+	}
+	free(s1);
+	return (ret);
+}
+
+char	*join_suffix(char *s1, char *s2)
+{
+	char	*ret;
+
+	ret = ft_strjoin(s1, s2);
+	if (!ret)
+	{
+		free (s1);
+		free (s2);
+		return (NULL);
+	}
+	free(s1);
+	return (ret);
+}
+
+char	*expand_variable(char *str)
+{
+	char	*prefix;
+	char	*prefix_value;
+	char	*final_result;
+	char	*value;
+	char	*var;
 	int		len_before;
 	int		len_after;
+
+	len_before = ft_strcspn(str, "$");
+	prefix = ft_substr(str, 0, len_before);
+	if (!prefix)
+		return (NULL);
+	var = parse_var_found(str);
+	value = get_var_value(var);
+	prefix_value = join_prefix_and_value(prefix, value);
+	if (!prefix_value)
+		return (NULL);
+	len_after = len_before + ft_strlen(var) + 1;
+	final_result = join_suffix(prefix_value, str + len_after);
+	if (!final_result)
+		return (NULL);
+	free(var);
+	free (value);
+	return (final_result);
+}
+
+void	handle_environment_vars_expansion(t_args_lst **arg_lst)
+{
 	t_args_lst	*args;
 
-	expanded_var = NULL;
-	var = NULL;
-	value = NULL;
 	args = *arg_lst;
 	while (args)
 	{
-		while (ft_strchr(args->arg, '$')) // the position needs to be saved
+		while (ft_strchr(args->arg, '$'))
 		{
 			if (args->arg[0] == '\'')
 				break ;
-			// while here because it might have multiple $ in a single string
-			len_before = ft_strcspn(args->arg, "$");
-			expanded_var = ft_substr(args->arg, 0, len_before);
-			var = parse_var_found(args->arg);
-			value = ft_getenv(var);
-			if (value == NULL)
-				value = ft_strdup("");
-			expanded_var = ft_strjoin(expanded_var, value);
-			len_after = len_before + ft_strlen(var) + 1;
-			expanded_var = ft_strjoin(expanded_var, args->arg + len_after);
-			args->arg = expanded_var;
+			args->arg = expand_variable(args->arg);
 		}
 		args = args->next;
 	}
