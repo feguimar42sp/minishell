d27e1ab7b4afb5d2ce217e4fd53c73aec304d146
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-11-04 21:24:28 -0300

    ctrl + \ and ctrl + c is working outside interactive mode

--- handle_signals.c
+++ handle_signals.c
@@ -6,7 +6,7 @@
 /*   By: sabrifer <sabrifer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 13:56:29 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/11/03 13:57:06 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/11/04 19:23:36 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,13 @@ void	handle_sigquit_signal(void)
 void	handle_sigint_signal(int sig)
 {
 	(void)sig;
-	write(1, "\n", 1);
-	get_prompt();
-	return ;
+
+	write(1, "\n", 1); // write a new line
+	rl_on_new_line(); // start a new line, kind of starts new and
+					  // ignores what was written before ctrl + c was pressed
+	rl_replace_line("", 0); // because line was stored in the buffer
+							// this one replaces what was stored with the text passed as argument
+	rl_redisplay(); // display prompt again
 }
 
 void	handle_signals(void)
@@ -30,7 +34,6 @@ void	handle_signals(void)
 	struct sigaction	action;
 	// handles ctrl + \ by ignoring it
 	handle_sigquit_signal();
-
 	// handles ctrl + c by doing nothing
 
 	action.sa_handler = handle_sigint_signal;
