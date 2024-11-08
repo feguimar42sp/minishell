f72ec76e858ab09312cfba7b769b1ad4cc7ab566
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-11-07 23:56:44 -0300

    static for exit status of commands added

--- statics/current_exit_code.c
+++ statics/current_exit_code.c
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   statics.h                                          :+:      :+:    :+:   */
+/*   current_exit_code.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sabrifer <sabrifer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/02 11:09:05 by fernando          #+#    #+#             */
-/*   Updated: 2024/11/02 11:23:56 by fernando         ###   ########.fr       */
+/*   Created: 2024/11/07 15:13:34 by sabrifer          #+#    #+#             */
+/*   Updated: 2024/11/07 15:14:33 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-#ifndef STATICS_H
-#define STATICS_H
+int	*current_exit_code(void)
+{
+    static int	exit_code;
 
-t_args_lst	**args_list(void);
-t_envp_lst 	**env_vars_list(void);
-t_mem_node	**mem_list(void);
-int			*current_exit_code(void);
-
-#endif
+    return (&exit_code);
+}
