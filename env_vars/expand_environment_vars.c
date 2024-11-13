d276d048916d540c797f07a35eef29b8d7356754
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-11-13 13:37:02 -0300

    tried to fix segfault when encountering a lone dollar sign

--- env_vars/expand_environment_vars.c
+++ env_vars/expand_environment_vars.c
@@ -6,7 +6,7 @@
 /*   By: sabrifer <sabrifer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:00:57 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/11/07 17:37:25 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/11/13 13:35:54 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,35 +23,40 @@ char	*parse_var_found(char *str)
 	i = 0;
 	start = 0;
 	len = 0;
-	while (str[i] != '$' && str[i] != '\0')
+	while (str[i] != '$' && str[i] != '\0') // skip until $ is found
 		i++;
-	if (str[i] == '$')
+	if (str[i] == '$') // set start of str and increase len by 1
 	{
 		i++;
 		start = i;
 		len++;
 	}
-	while (str[i] != '\0')
+	else
+		return (NULL);
+	if (str[i] == ' ' || str[i] == '\0') // if it finds a space or null, then print the dollar sign
 	{
-		if (str[i] == ' ' && str[i] != '\0')
-		{
-			len--;
-			break ;
-		}
-		else if (str[i] == '?')
-		{
-			len++;
-			break ;
-		}
-		else if (!ft_isalnum(str[i]) && str[i] != '_' && str[i] != '\0')
-		{
-			len--;
-			break ;
-		}
 		len++;
-		i++;
+		var_found = ft_strdup("$"); 
 	}
-	var_found = ft_substr(str, start, len);
+	else if (str[i] == '?')
+	{
+		len++;
+		var_found = ft_substr(str, start, len);
+	}
+	else
+	{
+		while (str[i] != '\0') // while to find the length of substr
+		{
+			if (!ft_isalnum(str[i]) && str[i] != '_' && str[i] != '\0')
+			{
+				len--;
+				break ;
+			}
+			len++;
+			i++;
+		}
+		var_found = ft_substr(str, start, len);
+	}
 	return (var_found);
 }
 
@@ -62,6 +67,11 @@ char	*get_var_value(char *var)
 	if (ft_strcmp(var, "?") == 0)
 	{
 		value = ft_itoa(*current_exit_code());
+		return (value);
+	}
+	else if (ft_strcmp(var, "$") == 0)
+	{
+		value = ft_strdup(var);
 		return (value);
 	}
 	value = ft_strdup(ft_getenv(var));
@@ -81,6 +91,7 @@ char	*expand_variable(char *str)
 	prefix = get_prefix(str);
 	var = parse_var_found(str);
 	value = get_var_value(var);
+	printf("value found = [%s]\n", value);
 	prefix_value = join_prefix_and_value(prefix, value);
 	free (value);
 	result = get_final_joined_str(str, prefix, prefix_value, ft_strlen(var));
@@ -89,7 +100,7 @@ char	*expand_variable(char *str)
 	free(var);
 	return (result);
 }
-
+// needs to fix when there's a lone '$' and it segfaults
 void	handle_environment_vars_expansion(t_args_lst **arg_lst)
 {
 	t_args_lst	*args;
