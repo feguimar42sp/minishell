615e566c4edc8d3dacd7afe43a631f049d189834
Feguimar <feguimar@student.42sp.org.br>
2024-12-03 10:21:41 -0300

    cd e pwd

--- run_commands/built-ins/ft_cd.c
+++ run_commands/built-ins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:35:44 by fernando          #+#    #+#             */
-/*   Updated: 2024/12/01 23:17:12 by fernando         ###   ########.fr       */
+/*   Updated: 2024/12/03 10:11:52 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 void	ft_cd(char **argv)
 {
-	chdir(argv[1]);
+	char	*old_pwd;
+	char	*temp;
+	char	*pwd;
+
+	temp = getcwd(NULL, 0);
+	old_pwd = ft_strjoin("OLDPWD=", temp);
+	free(temp);
+	if (chdir(argv[1]) == 0)
+	{
+		add_env_var(old_pwd);
+		temp = getcwd(NULL, 0);
+		pwd = ft_strjoin("PWD=", temp);
+		add_env_var(pwd);
+		free(temp);
+		free(pwd);
+	}
+	free(old_pwd);
 }
--- run_commands/built-ins/ft_pwd.c
+++ run_commands/built-ins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 20:34:37 by fernando          #+#    #+#             */
-/*   Updated: 2024/10/24 20:56:58 by fernando         ###   ########.fr       */
+/*   Updated: 2024/12/03 10:17:48 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,10 @@
 
 void	ft_pwd(char **argv)
 {
-	char	*resizable_buffer;
-	char	*valid_working_dir_return;
-	int		characters_in_wd_string;
+	char	*pwd;
 
-	if (argv == NULL)
-		return ;
-	characters_in_wd_string = 100;
-	valid_working_dir_return = NULL;
-	while (valid_working_dir_return == NULL)
-	{
-		resizable_buffer = malloc(sizeof(char) * characters_in_wd_string);
-		valid_working_dir_return = getcwd(resizable_buffer, characters_in_wd_string);
-		if ((valid_working_dir_return != NULL) || (characters_in_wd_string > 1073741820))
-			break ;
-		free(resizable_buffer);
-		characters_in_wd_string = characters_in_wd_string * 2;
-	}
-	if (characters_in_wd_string > 1073741820)
-		printf("Unable to allocate memory for PWD\n");
-	else
-		printf("%s\n", resizable_buffer);
-	free(resizable_buffer);
-	return ;
+	if (argv != NULL)
+		pwd = getcwd(NULL, 0);
+	printf("%s\n", pwd);
+	free(pwd);
 }
