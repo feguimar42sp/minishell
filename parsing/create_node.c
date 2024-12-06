f556808aa18d843c3c021e506c493897f3177940
Sabrina Ferreira Nascimento <sabrinaferreiran@gmail.com>
2024-12-06 18:03:23 -0300

    fixed leak in allocation of nodes

--- parsing/create_node.c
+++ parsing/create_node.c
@@ -6,7 +6,7 @@
 /*   By: sabrifer <sabrifer@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:03:21 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/11/03 14:03:27 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/12/06 18:02:57 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 void	create_node(t_args_lst **args_lst, const char *str, int start, int len)
 {
-	t_args_lst	*new;
+	t_args_lst	*new_lst;
 	t_args_lst	*temp;
 
-	new = (t_args_lst *)ft_lstnew(NULL);
+	new_lst = (t_args_lst *)malloc(sizeof(t_args_lst));
 	temp = *args_lst;
-	new->arg = ft_substr(str, start, len);
-	new->type = string;
-	new->next = NULL;
+	new_lst->arg = ft_substr(str, start, len);
+	new_lst->type = string;
+	new_lst->next = NULL;
 	if (temp == NULL)
-		*args_lst = new;
+		*args_lst = new_lst;
 	else
 	{
 		while (temp->next != NULL)
 			temp = temp->next;
-		temp->next = new;
-		new->next = NULL;
+		temp->next = new_lst;
+		new_lst->next = NULL;
 	}
 }
