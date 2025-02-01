9442594a46c6586c2118cf44962941679c402ca3
Fernando Guimaraes Loula <feguimar@c2r9p6.42sp.org.br>
2025-02-01 12:28:20 -0300

    heredoc bom

--- run_commands/heredoc/heredoc.c
+++ run_commands/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: feguimar <feguimar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 16:30:07 by feguimar          #+#    #+#             */
-/*   Updated: 2025/02/01 12:22:17 by feguimar         ###   ########.fr       */
+/*   Updated: 2025/02/01 12:26:59 by feguimar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,10 @@ void	heredoc(t_command *command, t_args_lst **ptr)
 			write_stderr("Warning: here-document ended by EOF",1);
 			break ;
 		}
-		printf("test line  *%s*\n", line);
 		line[ft_strlen(line) - 1] = '\0';
-		printf("test line no nl  *%s*\n", line );
 		if ((line != NULL) && ((ft_strcmp((*ptr)->arg, line) != 0)))
 		{
-			printf("test line no nl in loop  *%s*\n", line);
-			line[ft_strlen(line) - 1] = '\n';
-			printf("test final line  *%s*\n", line);
+			line[ft_strlen(line)] = '\n';
 			write(command->here[1], line, ft_strlen(line));
 		}
 		else
